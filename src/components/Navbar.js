179c4a95492a133a5f8ca47cdfcc597157f874fd
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-06-14 15:04:20 +0530

    Added logo in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,19 +15,22 @@ export default function Navbar() {
   return (
     <nav
       className={`fixed w-full z-50 top-0 left-0 transition-all duration-300 ${
-        isScrolled ? "bg-navbar-scrolled shadow-md" : "bg-transparent"
+        isScrolled ? "bg-[var(--color-navbar-scrolled)] shadow-md" : "bg-transparent"
       }`}
     >
       <div className="container mx-auto px-6 md:px-12 lg:px-20 flex justify-between items-center h-16">
-        <a href="#hero" className="text-xl font-bold text-primary">
-          Vaduni Niranjan
+        {/* Logo*/}
+        <a href="#hero" className="flex items-center gap-2 text-xl font-bold text-[var(--color-primary)]">
+          <img src="/logo.png" alt="Logo" className="w-40 h-25 object-contain" />
         </a>
+
+        {/* Navigation Links */}
         <ul className="hidden md:flex space-x-8 text-gray-700 font-medium">
           {["Home", "About", "Projects", "Skills", "Certifications", "Contact"].map((section) => (
             <li key={section}>
               <a
                 href={`#${section.toLowerCase()}`}
-                className="hover:text-primary transition-colors"
+                className="hover:text-[var(--color-primary)] transition-colors"
               >
                 {section}
               </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: ['bg-navbar-scrolled'],
   theme: {
     extend: {
       colors: {
-        primary: "#023047",
-        'navbar-scrolled': "#c89f9c",
+        primary: "#023047", 
+        
       },
     },
   },
